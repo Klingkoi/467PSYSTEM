55d053c3c8b96eb8e325dd79101e1a69d749f47e
DivineQi <105023943+DivineQi@users.noreply.github.com>
2024-12-01 13:07:45 -0600

    "Near" final version of adminPanel
    Just missing the order details :/

--- adminPanel.php
+++ adminPanel.php
@@ -9,7 +9,7 @@
   }
   catch (PDOexception $e) {
     echo "Connection to database failed: ".$e->getMessage();
-  } 
+  }
 ?>
 
 <html>
@@ -27,11 +27,24 @@
         .div3 {
             border:none;
             background-color:silver;
-            text-align:left;
+            text-align:center;
             visibility:collapse;
         }
-        .div4 {
-            text-align: center;
+        .content {
+            position: relative;
+            background: white;
+            padding: 10px;
+        }
+        .modal {
+            position: fixed;
+            inset: 0;
+            background: rgba(254,126,126,0.7);
+            display: none;
+            align-items: center;
+            justify-content: center;
+        }
+        .modal:target {
+            display: flex;
         }
 
     </style>
@@ -62,7 +75,6 @@
         background-color:white;
         width:25%;
     }
-
     table.center {
         margin-left: auto; 
         margin-right: auto;
@@ -89,7 +101,7 @@
     <!-- Brackets Table View -->
     </div> </br >
 
-    <div class="div4" id="bracketView">
+    <div class="div2" id="bracketView">
     <table class="center" style="width:50%;" id="bracketTable">
         <!-- Table headers -->
         <th colspan="7" style="background-color:White">Weight Brackets</th>
@@ -139,20 +151,20 @@
     <!-- Orders Table View-->
     
     <!-- Sort by Options -->
-    <div class="div3" id="SortBy">
-        <form action="" method="post">
-                <p style="text-align:center;">Adjust search: </p>
-                <!-- // Date Search -->
+        <div class="div3" id="SortBy">
+            <form action="" id="sortingForm" method="get">
+                <p>Adjust search: </p>
+                    <!-- // Date Search -->
                 <label for="dateStart">Dates from: </label>
                 <input type="date" id="dateStart" name="dateStart" value="2000-01-01"/>
                 <label for="dateEnd"> to: </label>
                 <input type="date" id="dateEnd" name="dateEnd" value="2025-01-01"/>
-                <!-- // Price Search -->
+                    <!-- // Price Search -->
                 <label for="priceStart"> Totals from: </label>
-                <input type="number" id="priceStart" name="priceStart" value="0" min="0"/>
+                <input type="number" id="priceStart" name="priceStart" value="0" min="0" max="99998"/>
                 <label for="priceEnd"> to: </label>
                 <input type="number" id="priceEnd" name="priceEnd" value="99999" min="0" max="99999"/>
-                <!-- Order Status Search -->
+                    <!-- Order Status Search -->
                 <label for="status"> Status:  </label>
                 <select type="text" id="status" name="status" value="All">
                     <option value="All">All</option>
@@ -162,16 +174,95 @@
                     <option value="Completed">Completed</option>
                 </select>
                 <input type="submit" id="submitted" name="submitted" value ="Search"/>
-        </form>
-    </div>
+            </form>
+        </div>
+        <div class="div3" id="Orders">
             <?php
-            // Add a new bracket limiter
-            if(isset($_POST["bracket"]) && isset($_POST["cost"])) {
-                $pdo->exec("INSERT INTO Brackets VALUES(".$_POST["bracket"].", ".$_POST["cost"].");");
+                if(isset($_GET["dateStart"])) {
+                    $dStart = $_GET["dateStart"];
+                    $dEnd = $_GET["dateEnd"];
+                    $pStart = $_GET["priceStart"];
+                    $pEnd = $_GET["priceEnd"];
+                    $status = $_GET["status"];
+                    if($status == "All") {
+                        $sql = "SELECT order_id, order_date, order_status, total_price FROM orders WHERE order_date >= '$dStart' AND order_date <= '$dEnd' AND total_price >= $pStart AND total_price <= $pEnd";
+                        $nSQL = "SELECT count(*) FROM orders WHERE order_date >= '$dStart' AND order_date <= '$dEnd' AND total_price >= $pStart AND total_price <= $pEnd";
+                    }
+                    else {
+                        $sql = "SELECT order_id, order_date, order_status, total_price FROM orders WHERE order_date >= '$dStart' AND order_date <= '$dEnd' AND total_price >= $pStart AND total_price <= $pEnd AND order_status = '$status'";
+                        $nSQL = "SELECT count(*) FROM orders WHERE order_date >= '$dStart' AND order_date <= '$dEnd' AND total_price >= $pStart AND total_price <= $pEnd AND order_status = '$status'";
+                    }
+                    $prepared = $pdo->prepare($sql);
+                    $prepared->execute();
+                    $row = $prepared->fetch(PDO::FETCH_NUM);
+                    echo "<table>";
+                    while($row) {
+                        echo "<tr>";
+                            echo "<td>Order Number: $row[0]($row[2]) </td>";
+                            echo "<td>$row[1] </td>";
+                            echo "<td>Total(w/ Shipping): $row[3] </td>";
+                            echo "<td> 
+                                    <div class=\"button\">
+                                        <a href=\"#popup-box-$row[0]\">Order Details $row[0]</a>
+                                    </div>
+                                </td>";
+                        echo "</tr>";
+                        $row = $prepared->fetch(PDO::FETCH_NUM);
+                    }
+                    echo "</table>";
+
+                    $prepared = $pdo->prepare($nSQL);
+                    $prepared->execute();
+                    $row = $prepared->fetch(PDO::FETCH_NUM);
+                    echo "<p style=\"text-align:left;\" >$row[0] order(s) returned!</p>";
+                    echo "</br >";
+
+                    # Detail Pop Up Box :)
+                    $preparedParts = $pdo->prepare("SELECT order_details.part_number, order_details.quantity,
+                                                                parts.description, parts.price
+                                                                FROM order_details 
+                                                                INNER JOIN parts ON parts.number = order_details.part_number
+                                                                WHERE order_details.order_id = $row[0]
+                                                                ORDER BY parts.number ASC");
+                    $preparedParts->execute();
+                    $partRow = $preparedParts->fetch(PDO::FETCH_NUM);   
+                    $preparedCustomer = $pdo->prepare("SELECT name, email, address FROM customers 
+                                                       WHERE customer_id = $row[4]");
+                    $preparedCustomer->execute();
+                    $custRow = $preparedCustomer->fetch(PDO::FETCH_NUM);   
+                    //Dynamic pop-up based on order
+                    echo "<div id=\"popup-box-$row[0]\" class=\"modal\"> 
+                            <div class=\"content\">
+                                <h1>Order $row[0]</h1>";
+                                echo "<h3>Packing List/Invoice:</h3>";
+                                while($partRow) {
+                                    $partTempTotal = $partRow[3] * $partRow[1];
+                                    echo "<p>$partRow[2] x $partRow[1] ($$partRow[3] x $partRow[1] = $$partTempTotal)</p>";
+                                    $partRow = $preparedParts->fetch(PDO::FETCH_NUM);
+                                }
+                                $subtotal = $row[1] - $row[5]; //total_price - shipping_cost
+                            echo "<p>Subtotal: $$subtotal</p>";
+                            echo "<p>Shipping: $$row[5]</p>";
+                            echo "<p>Total: $$row[1]</p>";
+                            echo "<h3>Shipping Confirmation:</h3>";
+                            echo "<p>$custRow[0]</p>";  //name
+                            echo "<p>$custRow[2]</p>";  //address
+                            echo "<p>Order confirmation sent to: $custRow[1]</p>";  //email
+                            echo "<a href=\"#\" style=\"position:absolute; color: #fe0606; top:10px; right:10px\">&times;</a> 
+                            </div>
+                        </div>";
+                    echo "</tr>";
                 }
-            // Remove a bracket limiter
-            else if(isset($_POST["rBracket"])) {
-                $pdo->exec("DELETE FROM Brackets WHERE bracket_upper = ".$_POST["rBracket"].";");
+            ?>
+        </div>
+            <?php
+                // Add a new bracket limiter
+                if(isset($_POST["bracket"]) && isset($_POST["cost"])) {
+                    $pdo->exec("INSERT INTO Brackets VALUES(".$_POST["bracket"].", ".$_POST["cost"].");");
+                }
+                // Remove a bracket limiter
+                else if(isset($_POST["rBracket"])) {
+                    $pdo->exec("DELETE FROM Brackets WHERE bracket_upper = ".$_POST["rBracket"].";");
                 }
             ?>
         </body>
--- js.php
+++ js.php
@@ -5,19 +5,28 @@
             var brackets = document.getElementById('bracketView');
             var bracketQs = document.getElementById('Qs');
             var ordersSort = document.getElementById('SortBy');
+            var orders = document.getElementById('Orders');
 
 
             if(brackets.style.display == 'none') {
                 brackets.style.display = 'inline';
                 bracketQs.style.display = 'inline';
                 ordersSort.style.visibility = 'collapse';
+                orders.style.visibility = 'collapse'
             }
             else {
                 brackets.style.display = 'none';
                 bracketQs.style.display = 'none';
                 ordersSort.style.visibility = 'visible';
+                orders.style.visibility = 'visible';
             }
         }
-    </script>
+      var tableView = document.getElementById("sortingForm");
+      function submitForm(event) {
+         event.preventDefault();
+      }
+      form.addEventListener('submit', submitForm);
+      toggleDisplay();
+      </script>
     </body>
 </html>
