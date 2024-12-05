046b4f33a90cece67dea747d30a1c89358d3d08b
Frogg6 <144403074+Frogg6@users.noreply.github.com>
2024-12-05 16:02:07 -0600

    Have Cart table

--- product.sql
+++ product.sql
@@ -1,8 +1,1 @@
-CREATE TABLE Inventory(part_ID int AUTO_INCREMENT, quantity int DEFAULT 0, PRIMARY KEY (part_ID));
 CREATE TABLE Cart(part_ID int, quantity int NOT NULL, PRIMARY KEY (part_ID));
-
--- Some temp data added --
-UPDATE Inventory SET quantity = 10 WHERE part_ID = 1;
-UPDATE Inventory SET quantity = 10 WHERE part_ID = 2;
-INSERT INTO Inventory VALUES (),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),(),();
-INSERT INTO Cart VALUES (2,2);
